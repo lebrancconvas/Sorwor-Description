41a290ee1014a8dc932c4b27fced8c442e5cf616
lebrancconvas <lebrancconvas@gmail.com>
2024-06-30 01:20:19 +0700

    ✨ feat: set header and footer

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+  <title>Sorwor (Senator) Description</title>
+</svelte:head>
+
+<main>
+  <header>
+    <h1>Sorwor (Senator) Description</h1>
+  </header>
+  <section id="input-section">
+
+  </section>
+  <section id="description-section">
+
+  </section>
+  <footer>
+    Copyright (c) 2023, by <a href="https://github.com/lebrancconvas" target="_blank">Poom Yimyuean (@lebrancconvas)</a> <a href="https://github.com/lebrancconvas/Sorwor-Description" target="_blank">Github</a>
+  </footer>
+</main>
+
+<style>
+  header > h1 {
+    text-align: center;
+  }
+  footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    text-align: center;
+    padding: 10px;
+  }
+</style>
